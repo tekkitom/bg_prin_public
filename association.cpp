a856d63df0336ea392c4f54b7de8d31c088f92d7
tekkitom <50363808+tekkitom@users.noreply.github.com>
2025-08-22 17:23:54 +0200

    Dateien verschoben

--- association.cpp
+++
@@ -1,8 +0,0 @@
-#include "association.h"
-
-Association::Association() {
-    for (int i = 0; i < maxCountMembersPatches; ++i) {
-        members[i] = nullptr;
-        patches[i] = nullptr;
-    }
-}
--- patch.cpp
+++
@@ -1,3 +0,0 @@
-#include "patch.h"
-
-Patch::Patch() {}
